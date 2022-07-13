30fd9290edfd1dce421f90f52599f0e20b054935
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2022-07-13 02:37:49 +0000

    new ⚛🐣

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return(
+        <React.Fragment>
+            <h1>Hello this is my youtube clone</h1>
+        </React.Fragment>
+    )
 }
 
 export default App;
