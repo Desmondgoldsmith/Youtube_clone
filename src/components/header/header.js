e1e3d057290ab6cd7291ac4762c89c363d2ffc1c
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2022-07-15 00:46:19 +0000

    Header Done ⚛

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,25 +3,25 @@ import './Header_.scss';
 import { FaBars } from 'react-icons/fa'
 import { AiOutlineSearch } from 'react-icons/ai'
 import { MdNotifications, MdApps } from 'react-icons/md'
-import { useHistory } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-const Header = ({ handleToggleSidebar }) => {
-    const [input, setInput] = useState('')
+// import { useHistory } from 'react-router-dom'
+// import { useSelector } from 'react-redux'
+const Header = () => {
+//     const [input, setInput] = useState('')
 
-    const history = useHistory()
+//     const history = useHistory()
  
-    const handleSubmit = e => {
-       e.preventDefault()
+//     const handleSubmit = e => {
+//        e.preventDefault()
  
-       history.push(`/search/${input}`)
-    }
-    const user = useSelector(state => state.auth?.user)
+//        history.push(`/search/${input}`)
+//     }
+//     const user = useSelector(state => state.auth?.user)
     return (<React.Fragment>
 <div className='header '>
          <FaBars
             className='header__menu'
             size={26}
-            onClick={() => handleToggleSidebar()}
+            // onClick={() => handleToggleSidebar()}
          />
 
          <img
@@ -30,12 +30,12 @@ const Header = ({ handleToggleSidebar }) => {
             className='header__logo'
          />
 
-         <form onSubmit={handleSubmit}>
+         <form>
             <input
                type='text'
                placeholder='Search'
-               value={input}
-               onChange={e => setInput(e.target.value)}
+            //    value={input}
+            //    onChange={e => setInput(e.target.value)}
             />
             <button type='submit'>
                <AiOutlineSearch size={22} />
@@ -45,7 +45,7 @@ const Header = ({ handleToggleSidebar }) => {
          <div className='header__icons'>
             <MdNotifications size={28} />
             <MdApps size={28} />
-            <img src={user?.photoURL} alt='avatar' />
+            {/* <img src={user?.photoURL} alt='avatar' /> */}
          </div>
       </div>
               </React.Fragment>);
