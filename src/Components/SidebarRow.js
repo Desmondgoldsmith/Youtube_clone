a83d299e632a8dc518aababc46e38abd86dff4a8
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-26 18:45:39 +0000

    Sidebar Section 🚀

--- src/Components/SidebarRow.js
+++ src/Components/SidebarRow.js
@@ -3,9 +3,9 @@ import './sidebar.css'
 
 function SidebarRow({selected,Icon,title}) {
   return (
-   <div className='side_row'>
-     <h3 className={`side_icon ${selected && "selected"}`}>{Icon}</h3>
-     <h3 className={`side_text`}>{title}</h3>
+   <div className={`side_row ${selected && "selected"}`}>
+     <h3 className = "side_icon">{Icon}</h3>
+     <h3 className = "side_text">{title}</h3>
    </div>
   )
 }
