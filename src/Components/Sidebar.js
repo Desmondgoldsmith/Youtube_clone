72301ceb54b2303397ba3da08ffc3371d93bfbb5
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-26 18:30:02 +0000

    Sidebar Section 🚀

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -6,8 +6,8 @@ import VideoCameraBackIcon from '@mui/icons-material/VideoCameraBack';
 import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
 function Sidebar() {
   return (
-    <div className='sidebar'>
-        <SidebarRow selected Icon={<HomeIcon/>} title = "Home"/>
+    <div className={'sidebar'}>
+        <SidebarRow selected Icon={<HomeIcon/>} title = "Home" className = "main" />
         <SidebarRow Icon={<VideoCameraBackIcon/>} title = "Shorts"/>
         <SidebarRow Icon = {<SubscriptionsIcon/>} title = "Subscription"/>
         
--- src/Components/SidebarRow.js
+++ src/Components/SidebarRow.js
@@ -5,7 +5,7 @@ function SidebarRow({selected,Icon,title}) {
   return (
    <div className='side_row'>
      <h3 className={`side_icon ${selected && "selected"}`}>{Icon}</h3>
-     <h3 className='side_text'>{title}</h3>
+     <h3 className={`side_text`}>{title}</h3>
    </div>
   )
 }
