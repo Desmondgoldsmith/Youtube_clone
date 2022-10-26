205a1fb96d434dcecc1e2faa1f78c8e8be194dea
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-26 15:49:26 +0000

    Sidebar Section 🚀

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Header from './Components/Header';
+import Sidebar from './Components/Sidebar';
 
 function App() {
   return (
     <div className="App">
+      {/* header section */}
       <Header />
+      {/* sidebar */}
+      <Sidebar />
+
     </div>
   );
 }
