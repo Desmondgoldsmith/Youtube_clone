af26b8f2aa19c4cc435b61e24f73e97c2360ad60
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-26 16:52:23 +0000

    Sidebar Section 🚀

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -2,14 +2,14 @@ import React from 'react'
 import './sidebar.css'
 import SidebarRow from './SidebarRow'
 import HomeIcon from '@mui/icons-material/Home';
-
-
+import VideoCameraBackIcon from '@mui/icons-material/VideoCameraBack';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
 function Sidebar() {
   return (
     <div className='sidebar'>
         <SidebarRow icon={<HomeIcon/>} title = "Home"/>
-        <SidebarRow title = "Trending"/>
-        <SidebarRow title = "Subscription"/>
+        <SidebarRow icon={<VideoCameraBackIcon/>} title = "Trending"/>
+        <SidebarRow icon = {<SubscriptionsIcon/>} title = "Subscription"/>
         
     </div>
   )
