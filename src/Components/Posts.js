660f0a3dbbe43fc34b8f573430b0828913c9d6e4
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-27 18:23:32 +0000

    Working on the search page  🚀

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import './posts.css'
 
 function Posts({title,keyword,views,timeStamp,image,name,tag,mainPic}) {
   return (
     <div className='posts'>
-      
-    </div>
+      <div className='image'>
+        <img src={mainPic} alt = {title}/>
+      </div>
+      <div className='content'>
+       <h2>{title}</h2>
+       <h3>{keyword}</h3>
+       <p>{views}⋅{timeStamp}</p>
+       <p><img src={image}/>{name}</p>
+       <span className = "tag">{tag}</span>
+</div>   
+ </div>
   )
 }
 
--- src/Components/Searchpage.js
+++ src/Components/Searchpage.js
@@ -28,8 +28,18 @@ function Searchpage() {
 
      </div>
      <hr/>
+     <h4>Latest Posts from Desmond Goldsmith</h4>
      <div className='posts'>
-       <Posts />
+       <Posts 
+        title = "Russian mercenaries build massive anti-tank defence"
+        keyword = "#POST"
+        views = "20M Views"
+        timeStamp = "2 hours ago"
+        image = "https://avatars.githubusercontent.com/u/62439177?s=400&u=4704bfad486fcb109ed0792b728093bdc647372c&v=4"
+        name = "Desmond Goldsmith"
+        tag = "New"
+        mainPic = "https://raw.githubusercontent.com/Desmondgoldsmith/Youtube_clone/main/public/vid1.png"
+       />
      </div>
     </div>
   )
