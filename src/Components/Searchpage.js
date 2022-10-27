5aae1f5d9fc639abb83a4bb1c40b6325fd5e7be4
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-27 17:20:38 +0000

    Adding the search page 🌬

--- src/Components/Searchpage.js
+++ src/Components/Searchpage.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import './searchpage.css'
+import TuneIcon from '@mui/icons-material/Tune';
 
 function Searchpage() {
   return (
     <div className = "searchpage">
-    Searchpage
+     <div className='filter'>
+      <TuneIcon  className='filter_icon'/>
+      Filters
+      <hr/>
+     </div>
+     <div className='profile_details'>
+       <div className='profile_pic'>
+       <img src='https://avatars.githubusercontent.com/u/62439177?s=400&u=4704bfad486fcb109ed0792b728093bdc647372c&v=4' alt='profile'/>
+       </div>
+        <div className='details'>
+            <h2>Desmond Goldsmith</h2>
+            <p>50k subscribers ⋅ 200 Videos </p>
+            <p>Official Youtube Account</p>
+        </div>
+
+     </div>
     </div>
   )
 }
