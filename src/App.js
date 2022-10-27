30ddfcbdd18959da4fb0e2c78bd4fc002acbd037
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-27 14:01:52 +0000

    Adding Videos 💥

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@ import './App.css';
 import Header from './Components/Header';
 import Sidebar from './Components/Sidebar';
 import Recommendation from './Components/Recommendation';
-import {BrowserRouter as  Router, Switch , Route} from "react-router-dom"
+import {BrowserRouter ,  Routes , Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
+     <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<h1>Home</h1>} />
+        <Route path = "/search" element = {<h1>Search</h1>} />
+      </Routes>
+     </BrowserRouter>
+
+
       {/* header section */}
-      <Header />
-      <div className='side_section'>
+      {/* <Header />
+      <div className='side_section'> */}
         {/* sidebar */}
-      <Sidebar />
-      <Recommendation />
-      </div>
+      {/* <Sidebar />
+      <Recommendation /> */}
+      {/* </div> */}
       
     </div>
   );
 }
 
 export default App;
-// npx i react-router-dom
+//npm install react-router-dom@6
