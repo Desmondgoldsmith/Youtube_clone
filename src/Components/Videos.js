ee18a83f6f9bcf7ed2dec8f44ad96b8d0688f99f
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-27 01:40:21 +0000

    Adding Videos 💥

--- src/Components/Videos.js
+++ src/Components/Videos.js
@@ -1,9 +1,25 @@
+import { Avatar } from '@mui/material'
 import React from 'react'
 import './video.css'
 
-function Videos({image,channel,time,views,timeStamp,ch_image}) {
+function Videos({image,title,channel,time,views,timeStamp,ch_image}) {
   return (
-    <div>Videos</div>
+    <div className='Videos'>
+      <img src={image} alt = "video" />
+      <div className='video_content'>
+        <Avatar className = "video_avatar"
+         alt = {channel}
+         src = {ch_image}
+        >{channel}
+        </Avatar>
+        <div className='video_text'>
+            <h4>{title}</h4>
+            <p>{channel}</p>
+            <p>{views} {timeStamp}</p>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
