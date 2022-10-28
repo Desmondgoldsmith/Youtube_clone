3eefe30d33aa0b45fa43eff230237d637251b3ba
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-28 03:45:32 +0000

    Working on the search page  🚀

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,6 +7,8 @@ import VideoCallIcon from '@mui/icons-material/VideoCall';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import AppsIcon from '@mui/icons-material/Apps';
 import { Link } from 'react-router-dom';
+import CloseIcon from '@mui/icons-material/Close';
+
 function Header() {
   const [search,setSearch] = useState("");
   return (
@@ -20,7 +22,8 @@ function Header() {
 
      <div className="all">
         <SearchIcon className = 'extra_search'/>
-                <input type="text" onChange={e=>setSearch(e.target.value)} value={search} placeholder='Search'/>
+                <input className="input_text" type="text" onChange={e=>setSearch(e.target.value)} value={search }  placeholder='Search'/>
+                <CloseIcon className="clear_r"/>
          <Link to={`/search/${search}`}>
               <SearchIcon className = 'searchButton'/>
           </Link>
