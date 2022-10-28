c380e4db20feb7a849f3fb44ee5775a960de8cc8
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-28 15:35:45 +0000

    Finishing Up 😎"

--- src/Components/Recommendation.js
+++ src/Components/Recommendation.js
@@ -45,24 +45,11 @@ export default class Recommendation extends Component{
      <div>
         <h2> Single Item</h2>
         <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
+          {data_carousel.map((data_carousel) => {
+            return(
+              
+            )
+          })}
         </Slider>
       </div>
      {/* end */}
